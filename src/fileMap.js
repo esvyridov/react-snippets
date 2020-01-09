2a1371a755d78ee8292d018467e71646e5e67044
esvyridov <esvyridov@solvd.com>
2020-01-09 17:37:00 +0300

    Add Prettier

--- src/fileMap.js
+++ src/fileMap.js
@@ -1,32 +1,32 @@
 const fileMap = {
-	javascript: {
-		index: {
-			input: "index.js.template",
-			output: "index.js"
-		},
-		css: {
-			input: "[CompName].module.css",
-			output: "[CompName].module.css"
-		},
-		component: {
-			input: "[CompName].jsx.template",
-			output: "[CompName].jsx"
-		}
-	},
-	typescript: {
-		index: {
-			input: "index.ts.template",
-			output: "index.ts"
-		},
-		css: {
-			input: "[CompName].module.css",
-			output: "[CompName].module.css"
-		},
-		component: {
-			input: "[CompName].tsx.template",
-			output: "[CompName].tsx"
-		}
-	}
-}
+  javascript: {
+    index: {
+      input: 'index.js.template',
+      output: 'index.js',
+    },
+    css: {
+      input: '[CompName].module.css',
+      output: '[CompName].module.css',
+    },
+    component: {
+      input: '[CompName].jsx.template',
+      output: '[CompName].jsx',
+    },
+  },
+  typescript: {
+    index: {
+      input: 'index.ts.template',
+      output: 'index.ts',
+    },
+    css: {
+      input: '[CompName].module.css',
+      output: '[CompName].module.css',
+    },
+    component: {
+      input: '[CompName].tsx.template',
+      output: '[CompName].tsx',
+    },
+  },
+};
 
 module.exports = fileMap;
